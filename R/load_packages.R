e59021039c2b34c0b00c8409a2ddc89bbd7643bf
Matthias Katzfuss <katzfuss@tamu.edu>
2018-03-09 16:43:06 -0600

    unsuccessful attempt at building a package

--- R/load_packages.R
+++
@@ -1,8 +0,0 @@
-###  required packages for the GPvecchia package
-
-library(GpGp)
-library(Matrix)
-library(RcppParallel)
-library(parallel)# in order to use detectCores
-library(sparseinv)
-library(fields) # only needed for pred.cond='independent'
