de52e4b32abe7a547fddd25e63bd306d06635c8c
Wenlong G <WenlongGong@gmail.com>
2018-03-23 14:38:47 -0500

    remove hello_world skeleton files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -73,49 +73,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _GPvecchia0_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _GPvecchia0_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _GPvecchia0_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _GPvecchia0_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_GPvecchia0_dist2", (DL_FUNC) &_GPvecchia0_dist2, 4},
@@ -123,10 +80,6 @@ static const R_CallMethodDef CallEntries[] = {
     {"_GPvecchia0_calcPWD2", (DL_FUNC) &_GPvecchia0_calcPWD2, 1},
     {"_GPvecchia0_calcPWD1", (DL_FUNC) &_GPvecchia0_calcPWD1, 1},
     {"_GPvecchia0_NZentries_new2", (DL_FUNC) &_GPvecchia0_NZentries_new2, 7},
-    {"_GPvecchia0_rcpparma_hello_world", (DL_FUNC) &_GPvecchia0_rcpparma_hello_world, 0},
-    {"_GPvecchia0_rcpparma_outerproduct", (DL_FUNC) &_GPvecchia0_rcpparma_outerproduct, 1},
-    {"_GPvecchia0_rcpparma_innerproduct", (DL_FUNC) &_GPvecchia0_rcpparma_innerproduct, 1},
-    {"_GPvecchia0_rcpparma_bothproducts", (DL_FUNC) &_GPvecchia0_rcpparma_bothproducts, 1},
     {NULL, NULL, 0}
 };
 
