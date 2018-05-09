74b456c7e6ff5c5a4e78078e16bae18e6def0a9a
Daniel Zilber <danielzilber@main-10-231-244-9.tamulink.tamu.edu>
2018-05-09 15:02:31 -0500

    Minor changes, explicitly defining default likelihood parameters related to issue #12.

--- vignettes/GPvecchia_laplace_vignette.R
+++ vignettes/GPvecchia_laplace_vignette.R
@@ -12,7 +12,9 @@ install_github("katzfuss-group/GPvecchia")
 
 data.distr = 'logistic' # options: "gaussian","logistic", "poisson", "gamma"
 spatial.dim = 1 # number of spatial dimensions
-n=10^2  # number of observed locs
+n=10^2 # number of observed locs
+
+default_lh_params = list("alpha"=2, "sigma"=.1)
 
 # simulate locations
 set.seed(12)
@@ -22,9 +24,8 @@ if(spatial.dim==1){
   locs <- cbind(runif(n),runif(n))
 }
 
-
 # covariance parameters
-sig2=1; range=.05; smooth=1
+sig2=1; range=.2; smooth=1.5
 covfun <- function(locs) sig2*Matern(fields::rdist(locs),range=range,smoothness=smooth)
 
 # simulate latent process
@@ -35,7 +36,7 @@ if(n < 1e4) {
 
 # simulate data
 if(data.distr=='gaussian'){
-  nuggets=rep(.1,n)
+  nuggets=rep(default_lh_params$sigma,n)
   z = rnorm(n,y,sqrt(nuggets))
 } else if(data.distr=='poisson'){
   z = rpois(n, exp(y))
@@ -62,24 +63,24 @@ if(spatial.dim==1) {
 m=2
 vecchia.approx=vecchia_specify(z, locs, m)
 
-
 #####################   prediction at observed locations    #######################
 
 covparms=c(sig2,range,smooth)
 # Perform inference on latent mean with Vecchia Laplace approximation
-posterior = calculate_posterior_VL(vecchia.approx, likelihood_model=data.distr, covparms)
+posterior = calculate_posterior_VL(vecchia.approx, likelihood_model=data.distr, covparms,likparms = default_lh_params)
 # Laplace approximation for comparison
 post_lap = calculate_posterior_laplace(z, data.distr, C = covfun(locs))
 
 if (spatial.dim==1){
   par(mfrow=c(1,1),mar=c(2,2,2,2))
-  ord = order(locs)
+  ord = order(locs) # order so that lines appear correctly
   plot(locs[ord], y[ord], type = "l")
-  lines(locs[ord], posterior$mean[ord], type = "l", col=3, lwd=3)
+  lines(locs[ord], posterior$mean[ord], type = "l", col=3, lwd=3, lty=1)
   lines(locs[ord], post_lap$mean[ord], type = "l", col=2)
   legend("topright", legend = c("Latent", "VL", "Laplace"), col= c(1,3,2), lwd=c(1,3,1))
 } else if (spatial.dim==2){
   par(mfrow=c(1,3), mar=c(2,3,2,3))
+  # ordering unnecessary; we are using a scatter plot rather than lines
   quilt.plot(locs, y, main= "Truth")
   quilt.plot(locs,posterior$mean,  main= "VL")
   quilt.plot(locs,array(post_lap$mean),  main= "Laplace")
