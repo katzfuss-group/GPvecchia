d46b099699d794bd3edfa7290a7d97c13f6e45f0
marcinjurek <marcinjurek1988@gmail.com>
2019-05-03 15:51:37 -0500

    update the mra vignette.R

--- vignettes/GPvecchia_mra_vignette.R
+++ vignettes/GPvecchia_mra_vignette.R
@@ -1,20 +1,11 @@
-rm(list=ls())
-rm(list=ls())
-setwd("/home/marcin")
-library(GpGp); library(Matrix); library(RcppParallel)
-library(parallel); library(sparseinv); library(fields)
-for (nm in list.files('GPvecchia/R',pattern = "\\.[RrSsQq]$")) {
-  #cat(nm,":");
-  source(file.path('GPvecchia/R',nm))#; cat("\n")
-}
-Rcpp::sourceCpp('GPvecchia/src/U_NZentries.cpp')
-Rcpp::sourceCpp('GPvecchia/src/MaxMin.cpp')
-Rcpp::sourceCpp('~/GPvecchia/src/fastTree.cpp')
+source("/home/marcin/GPvecchia/loader.r")
+
+#####################   simulate data    #######################
+
 
 set.seed(1988)
 spatial.dim=2# number of spatial dimensions
 n=400  # number of observed locs
-m=40
 
 # simulate locations
 if(spatial.dim==1){
@@ -22,10 +13,6 @@ if(spatial.dim==1){
 } else {
   locs = cbind(runif(n),runif(n))
 }
-
-# n=4^2
-# grid.oneside=seq(0,1,length=round(sqrt(n)))
-# locs=as.matrix(expand.grid(grid.oneside,grid.oneside))
 
 
 sig2=1; range=1; smooth=0.5
@@ -35,7 +22,6 @@ covfun = function(locs1, locs2=NULL) {
   if(is.null(locs2)){
     sig2*MaternFun(fields::rdist(locs1),covparms)
   } else {
-    #c(sig2*exp(-matrix(fields::rdist.vec(locs1, locs2), ncol=1)))
     c(sig2*MaternFun(matrix(fields::rdist.vec(locs1, locs2), ncol=1),covparms))
   }
 }
@@ -49,7 +35,8 @@ if(n <= 1e4) {
 } else z=rnorm(n)
 
 
-
+#####################   specify Vecchia approx    #######################
+#m=40
 # mra.options = list(plots=FALSE, r=c(0,16))
 # mra.options = list(plots=FALSE, M=4)
 # mra.options = list(plots=FALSE, r=c(0,11))
@@ -61,29 +48,88 @@ if(n <= 1e4) {
 # V = vecchia_specify(locs, m=3, conditioning='mra')#, mra.options=mra.options)
 # V = vecchia_specify(locs, m=20, conditioning = 'NN', cond.yz="SGV")
 #V = vecchia_specify(locs, m, conditioning = 'mra', mra.options = mra.options)
-V = vecchia_specify(locs, m, conditioning = 'mra')
+#V = vecchia_specify(locs, m, conditioning = 'mra')
 
 
 
-
-##### likelihood evaluation #####
+#####################   likelihood evaluation    #######################
 #Sig.sel = getMatCov(V, covfun(locs))
 
 #vecchia_loglik1 = vecchia_likelihood(z,V,covparms,nuggets,covmodel=Sig.sel)
 #vecchia_loglik2 = vecchia_likelihood(z,V,covparms,nuggets,covmodel=covfun)
-vecchia_loglik3 = vecchia_likelihood(z,V,covparms,nuggets,covmodel='matern')
+#vecchia_loglik3 = vecchia_likelihood(z,V,covparms,nuggets,covmodel='matern')
 
 
 
 # exact likelihood
-const = dim(locs)[1]*log(2*pi)
-logdet = as.numeric(determinant(Sigma, logarithm = TRUE)$modulus)
-quad.form2 = as.numeric(t(z) %*% solve(Sigma) %*% z)
-neg2loglike = const + logdet + quad.form2
-loglik = -neg2loglike/2
+#const = dim(locs)[1]*log(2*pi)
+#logdet = as.numeric(determinant(Sigma, logarithm = TRUE)$modulus)
+#quad.form2 = as.numeric(t(z) %*% solve(Sigma) %*% z)
+#neg2loglike = const + logdet + quad.form2
+#loglik = -neg2loglike/2
 
-print("Likelihood")
-print(paste("True: ",loglik,sep=""))
+#print("Likelihood")
+#print(paste("True: ",loglik,sep=""))
 #print(paste("Vecchia: ", vecchia_loglik1, sep=""))
 #print(paste("Vecchia: ", vecchia_loglik2, sep=""))
-print(paste("Vecchia: ", vecchia_loglik3, sep=""))
+#print(paste("Vecchia: ", vecchia_loglik3, sep=""))
+
+
+
+
+#####################   prediction   #######################
+
+######  specify prediction locations   #######
+n.p=30^2
+if(spatial.dim==1){  #  1-D case
+  locs.pred=matrix(seq(0,1,length=n.p),ncol=1)
+} else {   # 2-D case
+  grid.oneside=seq(0,1,length=round(sqrt(n.p)))
+  locs.pred=as.matrix(expand.grid(grid.oneside,grid.oneside)) # grid of pred.locs
+}
+n.p=nrow(locs.pred)
+
+
+######  specify Vecchia approximation   #######
+m=20
+vecchia.approx=vecchia_specify(locs,m,locs.pred=locs.pred, conditioning = 'mra')
+
+
+
+######  carry out prediction   #######
+# returns a list with elements mu.pred,mu.obs,var.pred,var.obs,V.ord
+preds=vecchia_prediction(z,vecchia.approx,covparms,nuggets)
+
+
+#####################   plot and compare to exact pred   #######################
+
+##  exact prediction
+mu.exact=as.numeric(covfun(rbind(locs,locs.pred))[,1:n]%*%solve(Sigma,z))
+cov.exact=covfun(rbind(locs,locs.pred))-
+  covfun(rbind(locs,locs.pred))[,1:n]%*%solve(Sigma,t(covfun(rbind(locs,locs.pred))[,1:n]))
+var.exact=diag(cov.exact)
+cov.exact.pred=cov.exact[n+(1:n.p),n+(1:n.p)]
+
+
+### plot Vecchia and exact predictions
+if(spatial.dim==1) {
+  plot(locs,z)
+  lines(locs.pred,preds$mu.pred,col='blue')
+  lines(locs.pred,preds$mu.pred-1.96*sqrt(preds$var.pred),col='blue',lty=2)
+  lines(locs.pred,preds$mu.pred+1.96*sqrt(preds$var.pred),col='blue',lty=2)
+  lines(locs.pred,mu.exact[n+(1:n.p)],col='red')
+  lines(locs.pred,mu.exact[n+(1:n.p)]-1.96*sqrt(var.exact[n+(1:n.p)]),col='red',lty=2)
+  lines(locs.pred,mu.exact[n+(1:n.p)]+1.96*sqrt(var.exact[n+(1:n.p)]),col='red',lty=2)
+} else {
+  sdrange=range(sqrt(c(preds$var.pred,var.exact[n+(1:n.p)])))
+  par(mfrow=c(2,3))
+  quilt.plot(locs,z)
+  quilt.plot(locs.pred,preds$mu.pred)
+  quilt.plot(locs.pred,sqrt(preds$var.pred),zlim=sdrange)
+  quilt.plot(locs,z)
+  quilt.plot(locs.pred,mu.exact[n+(1:n.p)])
+  quilt.plot(locs.pred,sqrt(var.exact[n+(1:n.p)]),zlim=sdrange)
+  par(mfrow=c(1,1))
+}
+
+
