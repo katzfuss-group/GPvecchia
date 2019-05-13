25a7b6b880a1d8b43cf077366f497a2771fe9c13
marcinjurek <marcinjurek1988@gmail.com>
2019-05-13 15:40:58 -0500

    this should be a fix to #48

---
+++ src/Matern.h
@@ -0,0 +1,16 @@
+#define ARMA_DONT_PRINT_ERRORS
+
+#include <iostream>
+#include <RcppArmadillo.h>
+#include <omp.h>
+#include <Rcpp.h>
+#include <boost/math/special_functions/bessel.hpp>
+#include <boost/math/special_functions/gamma.hpp>
+
+using namespace Rcpp;
+using namespace arma;
+using namespace std;
+
+
+
+mat MaternFun( mat distmat, vec covparms );
