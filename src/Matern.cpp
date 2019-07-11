8d7858b09a66c04716cb2967972417725ec6aea9
marcinjurek <marcinjurek1988@gmail.com>
2019-07-11 11:15:18 -0500

    add constants to Matern formulas to make it consistent with fields

--- src/Matern.cpp
+++ src/Matern.cpp
@@ -1,6 +1,8 @@
 #define ARMA_DONT_PRINT_ERRORS
+#define _USE_MATH_DEFINES
 
 #include <iostream>
+#include <math.h>
 #include <RcppArmadillo.h>
 #include <omp.h>
 #include <Rcpp.h>
@@ -34,24 +36,30 @@ if (covparms(2)==0.5) { // Exponential cov function
     }
   }
 } else if(covparms(2)==1.5) {
+  double alpha = covparms(1);
+  double normcon = (pow(alpha, -3)/sqrt(M_PI))*(boost::math::tgamma(2)/boost::math::tgamma(1.5));
   for (j1 = 0; j1 < d1; j1++){
     for (j2 = 0; j2 < d2; j2++){
       if ( distmat(j1,j2) == 0 ){
         covmat(j1,j2) = covparms(0);
       } else {
         scaledist = distmat(j1,j2)/covparms(1);
-        covmat(j1,j2) = covparms(0)*(1+sqrt(3)*scaledist)*exp(-sqrt(3)*scaledist);
+        //covmat(j1,j2) = covparms(0)*(1+sqrt(3)*scaledist)*exp(-sqrt(3)*scaledist);
+        double alpha = covparms(1);
+        covmat(j1,j2) = normcon*covparms(0)*0.5*M_PI*pow(alpha,3)*exp(-scaledist)*(1+scaledist);
       }
     }
   }
 } else if(covparms(2)==2.5){
+  double alpha = covparms(1);
+  double normcon = (pow(alpha, -5)/sqrt(M_PI))*(boost::math::tgamma(3)/boost::math::tgamma(2.5));
   for (j1 = 0; j1 < d1; j1++){
     for (j2 = 0; j2 < d2; j2++){
       if ( distmat(j1,j2) == 0 ){
         covmat(j1,j2) = covparms(0);
       } else {
         scaledist = distmat(j1,j2)/covparms(1);
-        covmat(j1,j2) = covparms(0)*(1+sqrt(5)*scaledist+(5.0/3.0)*scaledist*scaledist)*exp(-sqrt(5)*scaledist);
+        covmat(j1,j2) = normcon*covparms(0)*0.125*M_PI*pow(alpha,5)*exp(-scaledist)*(3+3*scaledist+scaledist*scaledist);
       }
     }
   }
