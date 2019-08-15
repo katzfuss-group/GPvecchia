80a3ec978815382f8435f772502e1e4562d269a5
marcinjurek <marcinjurek1988@gmail.com>
2019-08-15 10:05:34 -0500

    improve output of tests for parameter estimation

--- tests/testthat/test-parameter-estimation.r
+++ tests/testthat/test-parameter-estimation.r
@@ -1,11 +1,5 @@
-set.seed(1988)
 spatial.dim=2
 n=50
-if(spatial.dim==1){
-  locs=matrix(runif(n),ncol=1)
-} else {
-  locs <- cbind(runif(n),runif(n))
-}
 
 beta=2
 sig2=1; range=.1; smooth=1.5
@@ -13,15 +7,25 @@ covparms =c(sig2,range,smooth)
 covfun <- function(locs) sig2*MaternFun(fields::rdist(locs),covparms)
 nuggets=rep(.1,n)
 
-Om0 <- covfun(locs)+diag(nuggets)
+## data that gives too high smoothness
+set.seed(1989)
+locs_bad = cbind(runif(n),runif(n))
+
+Om0 <- covfun(locs_bad)+diag(nuggets)
 z=as.numeric(t(chol(Om0))%*%rnorm(n))
-data=z+beta
-
-vecchia.est=vecchia_estimate(data,locs)
+data_bad=z+beta
 
 
-#test_that("VL Posterior mean for fixed Poisson data returns expected values for first 3", {
-#  expect_equal(posterior$mean[1], 0.445597969)
-#  expect_equal(posterior$mean[2], 0.814020221)
-#  expect_equal(posterior$mean[3], -1.001667008)
-#})
+## data for which everything works
+set.seed(1988)
+locs_good = cbind(runif(n),runif(n))
+
+Om0 <- covfun(locs_good)+diag(nuggets)
+z=as.numeric(t(chol(Om0))%*%rnorm(n))
+data_good=z+beta
+
+
+test_that("VL Posterior mean for fixed Poisson data returns expected values for first 3", {
+  expect_error(vecchia_estimate(data_bad,locs_bad), "The smoothness parameter did not converge. Try a custom optimization routine.")
+  expect_output(vecchia_estimate(data_good,locs_good), "Exiting from Nelder Mead minimizer")
+})
